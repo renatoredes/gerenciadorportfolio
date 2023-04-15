4572a6ef39f2e6e64218cae1e3f8ca98f4e0651e
renatoredes <renatoredes@live.com>
2023-04-15 12:10:33 -0300

    configuração gitignore

---
+++ src/main/java/com/gerenciadorportfolio/gerenciadorportfolio/GerenciadorportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciadorportfolio.gerenciadorportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorportfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciadorportfolio/gerenciadorportfolio/GerenciadorportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadorportfolio.gerenciadorportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
